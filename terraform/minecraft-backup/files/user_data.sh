e3cc73b50f6d5a1b6f9162adbdaaa8a446fd57d6
Christopher M Wagner <70287489+Cwagne17@users.noreply.github.com>
2023-03-04 15:21:07 -0500

    change files

--- terraform/minecraft-backup/files/user_data.sh
+++ terraform/minecraft-backup/files/user_data.sh
@@ -4,12 +4,17 @@ wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.rpm
 sudo rpm -Uvh jdk-19_linux-x64_bin.rpm
 sudo yum -y install setools
 
-# minecraft
+# setup minecraft java
 sudo mkdir /minecraft
 sudo mkdir /minecraft/mc-server
 cd /minecraft
 sudo chown -R ec2-user:ec2-user /minecraft
 aws s3 cp --recursive s3://animeboys-minecraft-backup/mc-server/ mc-server/
+
+# setup minecraft forge
+sudo mkdir /minecraft/forge
+sudo chown -R ec2-user:ec2-user /minecraft/forge
+aws s3 cp --recursive s3://animeboys-minecraft-backup/forge/ forge/
 
 # systemd service
 sudo aws s3 cp s3://animeboys-minecraft-backup/setup/minecraft.service /etc/systemd/system
--- terraform/minecraft-backup/main.tf
+++ terraform/minecraft-backup/main.tf
@@ -18,17 +18,31 @@ resource "aws_s3_bucket_acl" "minecraft-backup" {
 resource "aws_s3_object" "server" {
   bucket = aws_s3_bucket.minecraft-backup.id
   key    = "mc-server/minecraft_server.1.19.3.jar"
-  source = "files/minecraft_server.1.19.3.jar"
-}
-
-resource "aws_s3_object" "forge" {
-  bucket = aws_s3_bucket.minecraft-backup.id
-  key = "mc-server/forge-1.18.2-40.2.0-installer.jar"
-  source = "files/forge-1.18.2-40.2.0-installer.jar"
+  source = "files/installers/minecraft_server.1.19.3.jar"
 }
 
 resource "aws_s3_object" "setup" {
     bucket = aws_s3_bucket.minecraft-backup.id
     key    = "setup/minecraft.service"
     source = "files/minecraft.service"
+}
+
+# Modded Minecraft Files
+
+resource "aws_s3_object" "forge" {
+  bucket = aws_s3_bucket.minecraft-backup.id
+  key = "forge/forge-1.18.2-40.2.0-installer.jar"
+  source = "files/installers/forge-1.18.2-40.2.0-installer.jar"
+}
+
+resource "aws_s3_object" "mantle" {
+    bucket = aws_s3_bucket.minecraft-backup.id
+    key    = "forge/mods/mantle-1.18.2-1.9.43.jar"
+    source = "files/mods/Mantle-1.18.2-1.9.43.jar"
+}
+
+resource "aws_s3_object" "tconstruct" {
+    bucket = aws_s3_bucket.minecraft-backup.id
+    key    = "forge/mods/tconstruct-1.18.2-3.6.3.111.jar"
+    source = "files/mods/TConstruct-1.18.2-3.6.3.111.jar"
 }
