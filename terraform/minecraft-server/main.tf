59972447350b421afa6d046680192550c28cf2ff
Christopher M Wagner <70287489+Cwagne17@users.noreply.github.com>
2023-03-04 13:13:03 -0500

    update user data

--- terraform/minecraft-server/main.tf
+++ terraform/minecraft-server/main.tf
@@ -3,20 +3,20 @@ locals {
 
     user_data = <<-EOT
     #!/usr/bin/env bash
-    sudo yum -y install java
+    # Install Java
+    wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.rpm
+    sudo rpm -Uvh jdk-19_linux-x64_bin.rpm
     sudo yum -y install setools
-    /* sudo systemctl start polkit */
 
     # minecraft
     sudo mkdir /minecraft
     sudo mkdir /minecraft/mc-server
     cd /minecraft
-    # for initial configuration enable copy from S3 cold storage to empty EBS:
-    aws s3 cp --recursive s3://<s3-bucket-name>/mc-server/ mc-server/
     sudo chown -R ec2-user:ec2-user /minecraft
+    aws s3 cp --recursive s3://animeboys-minecraft-backup/mc-server/ mc-server/
 
     # systemd service
-    sudo aws s3 cp s3://<s3-bucket-name>/setup/minecraft.service /etc/systemd/system
+    sudo aws s3 cp s3://animeboys-minecraft-backup/setup/minecraft.service /etc/systemd/system
     sudo systemctl daemon-reload
     sudo systemctl enable minecraft
     sudo systemctl start minecraft
