db4d74e589381497af02bcb7c2b4a3e03097f034
Josue Morales <molinajosue92@hotmail.com>
2023-03-05 15:59:29 -0500

    merge main and make some changes to the bot

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -3,6 +3,8 @@ import os
 import sys
 import dotenv
 import logging
+import boto3
+
 dotenv.load_dotenv()
 
 logger = logging.getLogger(__name__)
@@ -20,10 +22,25 @@ logging.basicConfig(
 AUTHORIZED_USERS = ['Faults#1644', 'ChrisW#6807']
 PRIVATE_CHANNEL = "minecraft-bot"
 
+KEY = os.environ.get("DISCORD_KEY")
+
+if KEY is None:
+    logger.info("Key not found")
+    sys.exit(1)
+
+INSTANCE_ID = os.environ.get("INSTANCE_ID")
+if INSTANCE_ID is None:
+    logger.error("Instance ID not found")
+    sys.exit(1)
+
+AWS_PROFILE = os.environ.get("AWS_PROFILE")
+ec2 = boto3.resource('ec2', region_name='us-east-1', profile_name=AWS_PROFILE)
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+client.run(KEY, log_handler=None)
 
 @client.event
 async def on_ready():
@@ -69,7 +86,28 @@ def print_help():
     """
 
 def start_minecraft_server():
-    pass
+    try:
+        res = ec2.start_instances(
+            InstanceIds=[INSTANCE_ID],
+            DryRun=False
+        )
+        curr_state = res['StartingInstances'][0]['CurrentState']['Name']
+        return f"Server's state is now {curr_state}"
+    except Exception as e:
+        logger.error(e)
+        return "An error occurred while starting the server. Please try again later."
+    
+def stop_minecraft_server():
+    try:
+        res = ec2.stop_instances(
+            InstanceIds=[INSTANCE_ID],
+            DryRun=False
+        )
+        curr_state = res['StoppingInstances'][0]['CurrentState']['Name']
+        return f"Server's state is now {curr_state}"
+    except Exception as e:
+        logger.error(e)
+        return "An error occurred while stopping the server. Please try again later."
     
 KEY = os.environ.get("DISCORD_KEY")
 
