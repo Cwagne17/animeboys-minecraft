369ebc48bab62bf682b93b18cdf898548935519d
Josue Morales <molinajosue92@hotmail.com>
2023-03-06 17:19:41 -0500

    updates to rust discord bot

--- animeboys-minecraft-bot/src/lib.rs
+++ animeboys-minecraft-bot/src/lib.rs
@@ -1,43 +1,116 @@
+use std::{fmt::Display, os};
+use std::env;
 use anyhow::anyhow;
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_ec2::{Error, Region};
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 use shuttle_secrets::SecretStore;
-use tracing::{error, info};
+use tracing::{error, info, debug};
+
+#[derive(Debug)]
+struct Ec2Error {
+    message: String
+}
+impl Display for Ec2Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+impl Ec2Error {
+    pub fn new(message: String) -> Ec2Error {
+        Ec2Error {
+            message
+        }
+    }
+}
+impl std::error::Error for Ec2Error {}
 
 const AUTHORIZED_USERS: [&str; 2] = ["Faults#1644", "ChrisW#6807"];
-
+const CHANNEL_ID: u64 = 1081598245358276721;
 struct Bot {
     /// The instance id of the ec2 instance
     instance_id: String,
     /// The profile to use when connecting to aws
-    aws_profile: String,
     ec2_client: aws_sdk_ec2::Client
 }
 
 impl Bot {
-    pub async fn new(instance_id: String, aws_profile: String) -> Bot {
+    pub async fn new(instance_id: String) -> Bot {
         // Create ec2 client
-        let config = aws_config::load_from_env().await;
-        let client = aws_sdk_ec2::Client::new(&config);
+        let config = aws_config::from_env().region(Region::new("us-east-1")).load().await;
+        let env_config = aws_config::environment::credentials::EnvironmentVariableCredentialsProvider::new();
+        let ec2_config_builder = aws_sdk_ec2::config::Builder::from(&config)
+        .credentials_provider(env_config)
+        .build();
+        let client = aws_sdk_ec2::Client::from_conf(ec2_config_builder);
         Bot {
             instance_id,
-            aws_profile,
             ec2_client: client
         }
     }
-    pub async fn start_instance(&self) {
-        todo!()
+    pub async fn start_instance(&self) -> Result<String, Ec2Error> {
+        let res = self.ec2_client
+                .start_instances()
+                .instance_ids(self.instance_id.clone())
+                .send()
+                .await
+                .map_err(|e| Ec2Error::new(e.to_string()))?;
+        Ok(res.starting_instances()
+            .unwrap()[0]
+            .current_state()
+            .unwrap()
+            .name()
+            .unwrap()
+            .as_str()
+            .to_string())
     }
-    pub async fn stop_instance(&self) {
-        todo!()
+    pub async fn stop_instance(&self) -> Result<(), Ec2Error> {
+        let res = self.ec2_client
+            .stop_instances()
+            .instance_ids(self.instance_id.clone())
+            .send()
+            .await
+            .map_err(|e| Ec2Error::new(e.to_string()))?;
+        Ok(())
     }
-    pub async fn get_instance_status(&self) {
-        todo!()
+    pub async fn get_instance_status(&self) -> Result<String, Ec2Error> {
+        let res = self
+            .ec2_client
+            .describe_instances()
+            .instance_ids(self.instance_id.clone())
+            .send()
+            .await
+            .map_err(|e| Ec2Error::new(e.to_string()))?;
+        let status = res
+            .reservations()
+            .ok_or(Ec2Error::new("No instances found".into()))?[0]
+            .instances()
+            .ok_or(Ec2Error::new("No instances found".into()))?[0]
+            .state()
+            .ok_or(Ec2Error::new("No state found".into()))?
+            .name()
+            .ok_or(Ec2Error::new("No name found".into()))?
+            .as_str()
+            .to_string();
+        Ok(status)
     }
-    pub async fn get_instance_ip(&self) {
-        todo!()
+    pub async fn get_instance_ip(&self) -> Result<String, Ec2Error> {
+        Ok(self.ec2_client
+            .describe_instances()
+            .instance_ids(self.instance_id.clone())
+            .send()
+            .await
+            .map_err(|e| Ec2Error::new(e.to_string()))?
+            .reservations()
+            .ok_or(Ec2Error::new("No reservations found".into()))?[0]
+            .instances()
+            .ok_or(Ec2Error::new("No instances found".into()))?[0]
+            .public_ip_address()
+            .ok_or(Ec2Error::new("No public ip found".into()))?
+            .to_string())
     }
     pub async fn print_help(&self) -> String {
         "
@@ -45,6 +118,7 @@ impl Bot {
         $start - Starts the Minecraft server
         $stop - Stops the Minecraft server
         $status - Gets the status of the Minecraft server
+        $getip - Gets the public ip of the Minecraft server
         $help - Displays this message
         ".into()
     }
@@ -53,27 +127,81 @@ impl Bot {
 #[async_trait]
 impl EventHandler for Bot {
     async fn message(&self, ctx: Context, msg: Message) {
+        // Ignore messages from self
+        if msg.author.bot {
+            return;
+        }
         // TODO: Add a way to authorize users
+        if !AUTHORIZED_USERS.contains(&msg.author.tag().as_str()) {
+            if let Err(e) = msg.channel_id.say(&ctx.http, "You are not authorized to use this bot").await {
+                error!("Error sending message: {:?}", e);
+            }
+            return;
+        }
+        debug!("Message received from {}", msg.author.tag());
         // TODO: Add a way to only accept messages from a specific channel
+        if msg.channel_id.0 != CHANNEL_ID {
+            info!("Message sent in wrong channel");
+            return;
+        }
+        debug!("Message received: {}", msg.content);
         match msg.content.as_str() {
             "$start" => {
-                if let Err(e) = msg.channel_id.say(&ctx.http, "Starting instance").await {
+                if let Err(e) = msg.channel_id.say(&ctx.http, "Starting instance...").await {
                     error!("Error sending message: {:?}", e);
                 }
+                let typing = msg.channel_id.start_typing(&ctx.http).unwrap();
+                let status = self.start_instance().await;
+                if status.is_err() {
+                    msg.channel_id.say(&ctx.http, "Error starting instance").await.unwrap();
+                    return;
+                }
+                let status = status.unwrap_or_else(|_| "undefined. Check Logs.".into());
+                if let Err(e) = msg.channel_id.say(&ctx.http, format!("The status of the instance is now: {}", status)).await {
+                    error!("Error sending message: {:?}", e);
+                }
+                typing.stop().unwrap();
+                    
             }
             "$stop" => {
-                if let Err(e) = msg.channel_id.say(&ctx.http, "Stopping instance").await {
+                if let Err(e) = msg.channel_id.say(&ctx.http, "Stopping instance...").await {
                     error!("Error sending message: {:?}", e);
                 }
+                let typing = msg.channel_id.start_typing(&ctx.http).unwrap();
+                if self.stop_instance().await.is_err() {
+                    msg.channel_id.say(&ctx.http, "Error stopping instance").await.unwrap();
+                    return;
+                }
+                if let Err(e) = msg.channel_id.say(&ctx.http, "The instance has been stopped").await {
+                    error!("Error sending message: {:?}", e);
+                }
+                typing.stop().unwrap();
             }
             "$status" => {
-                if let Err(e) = msg.channel_id.say(&ctx.http, "Instance status").await {
+                let status = self.get_instance_status().await;
+                if status.is_err() {
+                    msg.channel_id.say(&ctx.http, "Error getting instance status").await.unwrap();
+                    return;
+                }
+                let status = status.unwrap_or_else(|_| "undefined. Check Logs.".into());
+                if let Err(e) = msg.channel_id.say(&ctx.http, format!("The status of the instance is: {}", status)).await {
                     error!("Error sending message: {:?}", e);
                 }
             }
             "$getip" => {
-                if let Err(e) = msg.channel_id.say(&ctx.http, "Getting instance ip").await {
+                msg.channel_id.say(&ctx.http, "Getting instance ip...").await.unwrap();
+                let typing = msg.channel_id.start_typing(&ctx.http).unwrap();
+                let ip = self.get_instance_ip().await;
+                if ip.is_err() {
+                    msg.channel_id.say(&ctx.http, "Error getting instance ip").await.unwrap();
+                    return;
+                }
+                let ip = ip.unwrap_or_else(|_| "undefined. Check Logs.".into());
+                if let Err(e) = msg.channel_id.say(&ctx.http, format!("The ip of the instance is: {}", ip)).await {
                     error!("Error sending message: {:?}", e);
+                }
+                if let Err(e) = typing.stop().ok_or("error stopping typing") {
+                    error!("Error stopping typing: {:?}", e);
                 }
             }
             "$help" => {
@@ -81,11 +209,15 @@ impl EventHandler for Bot {
                     error!("Error sending message: {:?}", e);
                 }
             }
-            _ => {}
-        }
-        if msg.content == "!hello" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-                error!("Error sending message: {:?}", e);
+            "$hi" => {
+                if let Err(e) = msg.channel_id.say(&ctx.http, "Hello!").await {
+                    error!("Error sending message: {:?}", e);
+                }
+            }
+            _ => {
+                if let Err(e) = msg.channel_id.say(&ctx.http, "Unknown command. Try $help for a list of commands.").await {
+                    error!("Error sending message: {:?}", e);
+                }
             }
         }
     }
@@ -100,27 +232,17 @@ async fn serenity(
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> shuttle_service::ShuttleSerenity {
     // Get the discord token set in `Secrets.toml`
-    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
-        token
-    } else {
-        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
-    };
-    let instance_id = if let Some(instance_id) = secret_store.get("INSTANCE_ID") {
-        instance_id
-    } else {
-        return Err(anyhow!("'INSTANCE_ID' was not found").into());
-    };
-    let aws_profile = if let Some(aws_profile) = secret_store.get("AWS_PROFILE") {
-        aws_profile
-    } else {
-        return Err(anyhow!("'AWS_PROFILE' was not found").into());
-    };
+    env::set_var("AWS_ACCESS_KEY_ID", secret_store.get("AWS_ACCESS_KEY_ID").unwrap_or_default());
+    env::set_var("AWS_SECRET_ACCESS_KEY", secret_store.get("AWS_SECRET_ACCESS_KEY").unwrap_or_default());
+    env::set_var("AWS_DEFAULT_REGION", "us-east-1");
+    let token = secret_store.get("DISCORD_TOKEN").ok_or_else(|| anyhow!("'DISCORD_TOKEN' was not found"))?;
+    let instance_id = secret_store.get("INSTANCE_ID").ok_or_else(|| anyhow!("'INSTANCE_ID' was not found"))?;
 
     // Set gateway intents, which decides what events the bot will be notified about
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     let client = Client::builder(&token, intents)
-        .event_handler(Bot::new(instance_id, aws_profile).await)
+        .event_handler(Bot::new(instance_id).await)
         .await
         .expect("Err creating client");
 
